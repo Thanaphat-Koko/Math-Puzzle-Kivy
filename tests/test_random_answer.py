c3f499cbb74df76aa070e06934b5c48944c431f0
Thanaphat Pethdongjan <kokou102@hotmail.com>
2022-03-22 22:41:42 +0700

    TestRandomAns_Case1:Multi_0->10_must_in_0->100

--- tests/test_random_answer.py
+++ tests/test_random_answer.py
@@ -6,9 +6,11 @@ import random
 
 class Arithmetic_GetAnswerTest(unittest.TestCase):
 
-    # @patch('math_puzzle.arithmetric.Arithmetic.get_answer', return_value = 10)
-    def test_random_multiplication_0_to_10_must_less_than_101(self):
+    def test_random_multiplication_0_to_10_must_be_in_0_to_100(self):
         ari = Arithmetic(min_num=0, max_num=10)
         ari.get_multiplication_question()
         result = ari.get_answer()
-        self.assertLess(result, 101)
+        expected_result = [int(x) for x in range(100)]  #! 0 -> 100
+        self.assertIn(result, expected_result)
+
+
