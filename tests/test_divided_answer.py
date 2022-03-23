ec6607004729b80acc22dd53379e2178fa1cdaaa
Kamolchanok Chuchuen <6410110005@psu.ac.th>
2022-03-24 01:12:06 +0700

    test_24_divided_by_0_is_Divided_by_Zero

--- tests/test_divided_answer.py
+++ tests/test_divided_answer.py
@@ -30,3 +30,12 @@ class Arithmetic_GetAnswerTest(unittest.TestCase):
             result = ari.get_answer()
             expected_output = 1
             self.assertEqual(result, expected_output)
+
+    def test_24_divided_by_0_is_Divided_by_Zero(self):
+            ari = Arithmetic()
+            ari.num_one = 24
+            ari.num_two = 0
+            ari.operation = "÷"
+            result = ari.get_answer()
+            expected_output = "Divided by Zero"
+            self.assertEqual(result, expected_output)
